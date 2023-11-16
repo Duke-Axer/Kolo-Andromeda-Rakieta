40f90f947f2c760a4071fab8654aa893d31ee976
Brajan Gąbka <duke.axer@gmail.com>
2023-11-16 19:27:13 +0100

    Aktualizacja kodu

---
+++ Untitled-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+plik.open( "wyniki.txt" );
+const int rozmiar_wektorow=10000;
+struct kat 
+{
+  int pozycja[rozmiar_wektorow];
+  float kat_os_x[rozmiar_wektorow];
+  float kat_os_y[rozmiar_wektorow];
+};
+}
