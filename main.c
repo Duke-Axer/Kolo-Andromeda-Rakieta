023cab91cec4cee66da22d11bad5adac6d6ae58a
Brajan Gąbka <duke.axer@gmail.com>
2023-11-16 19:37:24 +0100

    Aktualizacja kodu

--- main.c
+++ main.c
@@ -4,81 +4,163 @@
 #define LED_PIN 4             // deklaracja wejść analogowych joysticka w osi X
 #define LED_COUNT 24 
 
-#define joyX A0             // deklaracja wejść analogowych joysticka w osi X
-#define joyY A1     
+#define joyX A0           // deklaracja wejść analogowych joysticka w osi X
+#define joyY A1           // deklaracja wejść analogowych joysticka w osi Y
 
-const int stepPin_obrot = 6; 
-const int dirPin_winda = 5; 
+const int red_button = 2;
+const int joystick_button = 3;
 
-const int stepPin_winda = 8; 
-const int dirPin_rakieta = 7; 
+const int dirPin_rakieta = 5; // deklaracja kierunku ruchu rakiety
+const int stepPin_rakieta = 6; // deklaracja pinu sygnałowego kroku rakiety
+const int dirPin_winda = 7;   // deklaracja kierunku ruchu windy
+const int stepPin_winda = 8;  // deklaracja pinu sygnałowego kroku windy
 
-int i = 0;
-const int kont_90 = 100;
+const int endstop_rakieta = 9;
+const int endstop_winda = 10;
 
-Adafruit_NeoPixel ring(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+unsigned long aktualnyCzas = 0;
+unsigned long aktualnyCzas2 = 0;
 
-  int licznik = 0;
+Adafruit_NeoPixel ring(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800); // deklacja pierściena ledowego
+
+int licznik = 0;
+int time_silniki_winda = 1;
+int time_silniki_rakieta = 1;
+
+bool kierunek_winda_memory = false;
+bool kierunek_rakieta_memory = false;
+
+void krok_rakieta();
+void krok_winda();
 
 void setup() {
- Serial.begin(9600);       // aktywowanie połączenia MCU <=> PC (USB)
+  Serial.begin(9600);       // aktywowanie połączenia MCU <=> PC (USB)
 
-  pinMode(stepPin_obrot,OUTPUT); 
+  pinMode(stepPin_winda,OUTPUT);  
   pinMode(dirPin_winda,OUTPUT);
 
-  pinMode(stepPin_winda,OUTPUT); 
+  pinMode(stepPin_rakieta,OUTPUT); 
   pinMode(dirPin_rakieta,OUTPUT);
 
+  pinMode(red_button,INPUT_PULLUP); // przycisk red button
+  pinMode(joystick_button,INPUT_PULLUP); // przycisk joysticka
+  pinMode(endstop_rakieta,INPUT_PULLUP); // krańcówka rakiety
+  pinMode(endstop_winda,INPUT_PULLUP); // krańcówka windy
+  pinMode(11,INPUT_PULLUP); // nie podpięte
+  pinMode(12,INPUT_PULLUP); // nie podpięte
+  pinMode(13,OUTPUT);       // sterowanie podświetleniem red button
 
   ring.begin();
   ring.show();
   ring.setBrightness(50);
 
+
+
 }
- 
 
 void loop() {
- int yValue = analogRead(joyX);  // odczytaj wartość z wejścia analogowego oś X joysticka
- int xValue = analogRead(joyY);  // odczytaj wartość z wejścia analogowego oś Y joysticka
+  int xValue = analogRead(joyX);  // odczytaj wartość z wejścia analogowego oś X joysticka
+  int yValue = analogRead(joyY);  // odczytaj wartość z wejścia analogowego oś Y joysticka
 
+  // Serial.print(digitalRead(endstop_winda));Serial.print("winda - rakieta\t");Serial.println(digitalRead(endstop_rakieta));
+//Serial.println(digitalRead(2));
 
-  if(xValue>600){digitalWrite(dirPin_winda,LOW); }
-  if(xValue<450){digitalWrite(dirPin_winda,HIGH); }
+///////// określanie kierunku ruchu silników //////////////
 
-    if(yValue>600){digitalWrite(dirPin_rakieta,LOW); }
-  if(yValue<450){digitalWrite(dirPin_rakieta,HIGH); }
-int time_silniki_obrot = 20;  // rakieta
-int time_silniki_winda = 1; // winda
+  if(yValue>600){kierunek_winda_memory = LOW; digitalWrite(dirPin_winda,kierunek_winda_memory); }
+  if(yValue<450){kierunek_winda_memory = HIGH; digitalWrite(dirPin_winda,kierunek_winda_memory); }
 
+  if(xValue>600){kierunek_rakieta_memory = HIGH; digitalWrite(dirPin_rakieta,kierunek_rakieta_memory); }
+  if(xValue<450){kierunek_rakieta_memory = LOW; digitalWrite(dirPin_rakieta,kierunek_rakieta_memory); }
+  
 
+///////////////////////////////////////////////////////////
+/////////////sterowanie windą /////////////////////////////
+///////////////////////////////////////////////////////////
+if (digitalRead(endstop_winda)==true)
+    {
+    if(yValue>600||yValue<450)
+      {
+   krok_winda();
+      }
+    }
+if (digitalRead(endstop_winda)==false && digitalRead(dirPin_winda) == LOW)
+    {
+krok_winda();
+    }
+///////////////////////////////////////////////////////////
+///////////////sterowanie rakietą//////////////////////////
+///////////////////////////////////////////////////////////
 
 if(xValue>600||xValue<450)
   {
-    for (i=0; i<kont_90;i++)
-{
-    digitalWrite(stepPin_obrot,HIGH); 
-    delay(time_silniki_obrot); 
-    digitalWrite(stepPin_obrot,LOW); 
-    delay(time_silniki_obrot); 
-   
-}
-  }
-  i=0;
-
-
-  if((xValue>600||xValue<450)&& false)
-  {
-    digitalWrite(stepPin_obrot,HIGH); 
-    delay(time_silniki_obrot); 
-    digitalWrite(stepPin_obrot,LOW); 
-    delay(time_silniki_obrot); 
-  }
-  if((yValue>600||yValue<450)&& false)
-  {
-    digitalWrite(stepPin_winda,HIGH); 
-    delay(time_silniki_winda); 
-    digitalWrite(stepPin_winda,LOW); 
-    delay(time_silniki_winda); 
+    digitalWrite(red_button,HIGH);
+    
+    krok_rakieta();
   
+    digitalWrite(red_button,LOW);
   }
+
+if (digitalRead(joystick_button)==false)
+{kierunek_rakieta_memory = !kierunek_rakieta_memory; digitalWrite(dirPin_rakieta,kierunek_rakieta_memory); 
+  /* code */
+////digitalWrite(2,HIGH);
+for(int i = ring.numPixels(); i >= -2; i--){
+    ring.setPixelColor(i, 100, 0, 0, 0);
+    ring.setPixelColor(i-1, 0, 100, 0, 0);
+    ring.setPixelColor(i-2, 0, 0, 0, 0);
+    ring.show();
+    delay(10);}
+do
+{
+ krok_rakieta();
+} while (digitalRead(endstop_rakieta)==false );
+for(int i = ring.numPixels(); i >= -2; i--){
+    ring.setPixelColor(i-2, 0, 0, 0, 0);
+    ring.show();
+    delay(10);}
+
 }
+
+//Serial.println(digitalRead(red_button));
+//////////////////////////////////////////////
+if (digitalRead(red_button)==0)
+{  if (licznik==1){aktualnyCzas = millis();}
+  licznik++;
+ if (licznik==100){aktualnyCzas2 = millis() - aktualnyCzas; Serial.println(aktualnyCzas2);}
+ if (licznik>=100)
+{
+    time_silniki_rakieta = 2;
+   for(int j= 1;j <=4; j++){for(int i = 0; i< 400; i++){krok_rakieta();}delay(250);}
+   aktualnyCzas2 = millis() - aktualnyCzas; Serial.println(aktualnyCzas2);
+   Serial.println(licznik);
+   Serial.println("Koniec obrotu");
+ }
+//Serial.println(licznik);
+}
+
+
+if (digitalRead(red_button)==1)
+{licznik = 0;}
+
+  }
+
+  void krok_rakieta()
+  {
+
+  digitalWrite(stepPin_rakieta,HIGH); 
+  delay(time_silniki_rakieta); 
+  digitalWrite(stepPin_rakieta,LOW); 
+  delay(time_silniki_rakieta); 
+
+  }
+
+   void krok_winda()
+  {
+
+  digitalWrite(stepPin_winda,HIGH); 
+  delay(time_silniki_winda); 
+  digitalWrite(stepPin_winda,LOW); 
+  delay(time_silniki_winda); 
+
+  }
